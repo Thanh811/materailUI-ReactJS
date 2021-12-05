4452e8a5f49d66757b61acfd12ff39c44a71007c
LeThanh <lethanh040997@gmail.com>
2021-12-05 13:17:15 +0700

    initial project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fixed>
+      <Box>
+        <Typography variant="h5" gutterBottom component="div">
+          1. Create a form which you can upload content the image file with some accompanying data fields
+        </Typography>
+        <Typography variant="h4" gutterBottom component="div">
+        Create new item
+      </Typography>
+      <Typography variant="subtitle2" gutterBottom component="div">
+        Image, Video, Audio, or 3D Modals
+      </Typography>
+      <Typography variant="body2" gutterBottom>
+        File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG, Max size: 100MB
+      </Typography>
+      {/* Image */}
+      <Typography variant="body2" gutterBottom>
+        Name
+      </Typography>
+      <TextField fullWidth label="Name Item" id="fullWidth" />
+      <Typography variant="body2" gutterBottom>
+        External Link
+      </Typography>
+      <TextField fullWidth label="Name Item" id="fullWidth" />
+      </Box>
+    </Container>
   );
 }
 
